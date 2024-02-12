01109dbea624700028dfcb90c1a0930fe91f77e4
jdeveloperanalyst <jonatas1200@icloud.com>
2024-02-12 10:26:40 -0300

    hotfix/study

--- 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
+++ 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
@@ -4,20 +4,25 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
 
     def listar_restaurantes():
+        nome_restaurante = 'Nome do Restaurante'
+        categoria = 'Categoria'
+        status = 'Status'
+        print(f'{nome_restaurante.ljust(25)} | {categoria.ljust(25)} | {status}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
     
-    
+    @property
+    def ativo(self):
+        return '☑' if  self._ativo else '☐'
     
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 Restaurante.listar_restaurantes()
-
