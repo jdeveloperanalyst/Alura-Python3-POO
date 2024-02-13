1b929c02573f598aa3583c13839d021224fcb3ae
jdeveloperanalyst <jonatas1200@icloud.com>
2024-02-13 11:39:36 -0300

    hotfix/decorador property

--- 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
+++ 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
@@ -2,13 +2,13 @@ class Restaurante:
     restaurantes = []
     
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
 
     def listar_restaurantes():
         nome_restaurante = 'Nome do Restaurante'
@@ -16,13 +16,13 @@ class Restaurante:
         status = 'Status'
         print(f'{nome_restaurante.ljust(25)} | {categoria.ljust(25)} | {status}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
     
     @property
     def ativo(self):
         return '☑' if  self._ativo else '☐'
     
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
