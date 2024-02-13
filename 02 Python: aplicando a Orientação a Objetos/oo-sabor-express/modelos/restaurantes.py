eebac42b74c11f22cd613a22b993d9705ac4c8ce
jdeveloperanalyst <jonatas1200@icloud.com>
2024-02-13 19:04:33 -0300

    hotfix/metodos de classe

--- 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
+++ 02 Python: aplicando a Orientação a Objetos/oo-sabor-express/modelos/restaurantes.py
@@ -10,19 +10,24 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
 
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         nome_restaurante = 'Nome do Restaurante'
         categoria = 'Categoria'
         status = 'Status'
         print(f'{nome_restaurante.ljust(25)} | {categoria.ljust(25)} | {status}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
     
     @property
     def ativo(self):
-        return '☑' if  self._ativo else '☐'
+        return '✅' if  self._ativo else '❌'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
     
 restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
